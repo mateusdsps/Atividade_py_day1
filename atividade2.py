eedcef1854ebbe9740eb84021f1573f2ba89fe38
Mateus <mateusdsps@gmail.com>
2026-01-08 21:06:40 -0300

    Minhas atividades de Python

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+# Definindo as variáveis
+numero1 = 12
+
+numero2 = 14
+
+# Realizando a soma
+resultado = numero1 + numero2
+
+# Resultado
+print(f"A soma de {numero1} com {numero2} é: {resultado}")
---
+++ atividade4.py
@@ -0,0 +1,18 @@
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+# Calculando o preço total
+preco_total = preco_unitario * quantidade
+
+# Exibindo o relatório da compra
+print("-" * 30)
+print("DETALHES DA COMPRA")
+print("-" * 30)
+print(f"Produto: {nome_produto}")
+print(f"Quantidade: {quantidade} unidades")
+print(f"Preço Unitário: R$ {preco_unitario:.2f}")
+print("-" * 30)
+print(f"VALOR TOTAL: R$ {preco_total:.2f}")
+print("-" * 30)
